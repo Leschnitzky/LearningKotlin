f8eca6c8afb0cfc493778593568987e9eac5b9f6
Sergey <leschinskysergey@gmail.com>
2020-04-24 20:21:51 +0300

    Added support for DataBinding on HomeFragment Added error message on an empty Username

--- app/src/main/java/com/example/learningkotlin/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/learningkotlin/ui/home/HomeFragment.kt
@@ -4,17 +4,26 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.example.learningkotlin.MainActivity
 import com.example.learningkotlin.R
+import com.example.learningkotlin.databinding.FragmentHomeBinding
+import com.google.android.material.textfield.TextInputLayout
 
 class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
-
+    private lateinit var editText: EditText
+    private lateinit var loginButton: Button
+    private lateinit var binding : FragmentHomeBinding
+    private lateinit var userInputLayout: TextInputLayout
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
@@ -22,7 +31,20 @@ class HomeFragment : Fragment() {
     ): View? {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-        return root
+        binding = FragmentHomeBinding.inflate(inflater, container,false)
+
+        userInputLayout = binding.textInputLayout
+        editText = binding.loginUserEditText
+        loginButton = binding.loginButton
+
+        loginButton.setOnClickListener {
+            if(editText.text.isEmpty()){
+                Toast.makeText(context,"Clicked Login Button",Toast.LENGTH_SHORT)
+                userInputLayout.isErrorEnabled = true
+                userInputLayout.error = "Username must not be empty"
+            }
+        }
+
+        return binding.root
     }
 }
